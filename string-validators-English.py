5898adfce4fcb5722e646c1763e331bfe50d3dd8
Tushar Rokade <95062408+rokadetushar@users.noreply.github.com>
2022-03-08 22:20:58 +0530

    Add files via upload

---
+++ string-validators-English.py
@@ -0,0 +1,20 @@
+s  =  input()
+alnm = 'False'
+alp = 'False'
+dig = 'False'
+low = 'False'
+upp = 'False'
+for i in s:
+    if i.isalnum():
+        alnm = 'True'
+    if i.isalpha():
+        alp = 'True'
+    if i.isdigit():
+        dig= 'True'
+    if i.islower():
+        low= 'True'
+    if i.isupper():
+        upp = 'True'
+print(alnm + '\n'+ alp + '\n'+dig + '\n'+ low+ '\n'+upp)
+
+    
